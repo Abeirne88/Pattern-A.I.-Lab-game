18427a5f21baf4ad4ffc7213e234551618e1bd93
Alan Beirne <Alanbeirne88@gmail.com>
2022-03-04 22:55:19 +0000

    2nd

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -6,19 +6,28 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+    //zero cherries 
 
     [SerializeField] private Text cherriesText;
+    //in case need to edit it later in the editor
 
-    [SerializeField] private AudioSource collectionSoundEffect;
+    //[SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
+        //when it collides
     {
         if (collision.gameObject.CompareTag("Cherry"))
+            //if it collides with cherry tag
         {
-            collectionSoundEffect.Play();
+            //collectionSoundEffect.Play();
+            //play sound
             Destroy(collision.gameObject);
+            //destriy object
             cherries++;
+            //add 1 to cherries
+            Debug.Log("Cherries: ");
             cherriesText.text = "Cherries: " + cherries;
+            //cherries goes up
         }
     }
 }
